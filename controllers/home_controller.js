1d7cd44179db355c766db5a4eb087956c4ed799b
abhishek rathee <abhishekuu10@gmail.com>
2021-05-09 19:29:39 +0530

    create another route for user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,3 +4,8 @@ module.exports.home=function(req,res){
 }
 
 // module.exports.actionName=(req,res)={}
+
+module.exports.home=function(req,res){
+
+    return res.end('<h1>Express is up</h1>');
+}
---
+++ controllers/user_controller.js
@@ -0,0 +1,14 @@
+module.exports.home=function(req,res){
+
+    res.end('<h1>User </h1>');
+}
+
+module.exports.profile=function(req,res){
+
+    res.end('<h1>User profile</h1>');
+}
+
+module.exports.posts=function(req,res){
+
+    res.end('<h1>User posts</h1>');
+}
